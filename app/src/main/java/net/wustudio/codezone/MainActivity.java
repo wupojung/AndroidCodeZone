9fb22de4f5f55c58b3bd550d74e5b73d042b9b1e
wupojung <wupojung@gmail.com>
2016-12-21 00:08:43 +0800

    動態新增Menu測試

--- app/src/main/java/net/wustudio/codezone/MainActivity.java
+++ app/src/main/java/net/wustudio/codezone/MainActivity.java
@@ -2,6 +2,10 @@ package net.wustudio.codezone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //取得ID
+        LinearLayout llMenu = (LinearLayout) findViewById(R.id.llMenu);
+
+        //按鈕
+        Button btn = new Button(this);
+        btn.setText("Test");//指定文字
+        btn.setOnClickListener(new View.OnClickListener() {//指定OnClick Event
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Test On Click", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //新增
+        llMenu.addView(btn);
     }
+
+
 }
