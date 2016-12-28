1ff9208781f6b102eca97c4e300066026a39d566
pojung.wu <pojung.wu@wowprime.com>
2016-12-28 15:46:02 +0800

    WebView  Setting Demo

--- app/src/main/java/net/wustudio/codezone/WebViewActivity.java
+++ app/src/main/java/net/wustudio/codezone/WebViewActivity.java
@@ -21,6 +21,19 @@ public class WebViewActivity extends Activity {
         btnHomePage.setOnClickListener(onHomePageClikc);
         //綁UI - WebView
         wvPage = (WebView) findViewById(R.id.wvPage);
+        //regon //設定WebView
+        //官方文件:  https://developer.android.com/reference/android/webkit/WebSettings.html
+        //其他設定：
+        wvPage.getSettings().setBuiltInZoomControls(true);//開啟內建縮放功能
+        wvPage.getSettings().setJavaScriptEnabled(true);//允許Javascript 生效
+
+        //cashe
+        //官方文件: https://developer.android.com/reference/android/webkit/WebSettings.html#LOAD_CACHE_ELSE_NETWORK
+        //wvPage.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK); //強制使用本地的Cache
+        wvPage.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);  //不使用cache
+        //wvPage.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ONLY);  //限定使用cache(不使用網路)
+        //wvPage.getSettings().setCacheMode(WebSettings.LOAD_DEFAULT); //預設，注意：沒有網路會爆錯 (沒給權限 = 沒有網路)
+        //endregion
     }
 
     View.OnClickListener onHomePageClikc = new View.OnClickListener() {
